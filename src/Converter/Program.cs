15b2a620f88d30a11237eaefe8cdfb2efb9fbed2
Akis Idrizi <aidrizi@outlook.com.gr>
2024-06-26 19:32:04 +0300

    Initial Commit

---
+++ src/Converter/Program.cs
@@ -0,0 +1,23 @@
+﻿using SVGtoWEBP;
+
+const string svgUrl = "https://www.svgrepo.com/download/288629/ram.svg";
+const string assetsDirectory = "src/assets";
+const int webpWidth = 500;
+const int webpHeight = 500;
+
+var directoryInfo = Directory.CreateDirectory(assetsDirectory);
+
+// Fetch SVG from URL
+var svgContent = await SvGtoWebp.FetchSvg(svgUrl);
+
+// Validate and save SVG
+if (SvGtoWebp.IsSvg(svgContent)) {
+	var svgPath = Path.Combine(assetsDirectory, $"{Guid.NewGuid()}.svg");
+	File.WriteAllText(svgPath, svgContent);
+
+	// Convert SVG to WEBP
+	var webpPath = Path.Combine(assetsDirectory, $"{Guid.NewGuid()}.webp");
+	SvGtoWebp.ConvertSvgToWebp(svgPath, webpPath, webpWidth, webpHeight);
+} else {
+	Console.WriteLine("Invalid SVG content.");
+}
---
+++ src/Domain/SvGtoWebp.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.RegularExpressions;
+using SkiaSharp;
+using Svg.Skia;
+
+namespace SVGtoWEBP;
+
+public static partial class SvGtoWebp {
+
+	public static async Task<string> FetchSvg(string url) {
+		using var client = new HttpClient();
+		var response = await client.GetAsync(url);
+		response.EnsureSuccessStatusCode();
+		return await response.Content.ReadAsStringAsync();
+	}
+
+	public static bool IsSvg(string content) {
+		return SvgRegex().IsMatch(content);
+	}
+
+	public static void ConvertSvgToWebp(string svgPath, string webpPath, int width, int height) {
+		using var stream = File.OpenRead(svgPath);
+		var svg = new SKSvg();
+		var picture = svg.Load(stream);
+
+		if (picture != null) {
+			using var bitmap = new SKBitmap(new SKImageInfo(width, height, SKColorType.Bgra8888, SKAlphaType.Unpremul));
+			using var canvas = new SKCanvas(bitmap);
+			canvas.Clear(SKColors.Transparent);
+
+			var xRatio = width / picture.CullRect.Width;
+			var yRatio = height / picture.CullRect.Height;
+			var ratio = Math.Min(xRatio, yRatio);
+			var x = (width - ratio * picture.CullRect.Width) / 2;
+			var y = (height - ratio * picture.CullRect.Height) / 2;
+
+			canvas.Scale(ratio);
+			canvas.Translate(x, y);
+			canvas.DrawPicture(picture);
+			canvas.Flush();
+
+			using var image = SKImage.FromBitmap(bitmap);
+			using var data = image.Encode(SKEncodedImageFormat.Webp, 100);
+			using var outputStream = File.OpenWrite(webpPath);
+			data.SaveTo(outputStream);
+		} else {
+			Console.WriteLine("Failed to load SVG.");
+		}
+	}
+
+	[GeneratedRegex(@"<svg[^>]*>(.*?)<\/svg>", RegexOptions.Singleline)]
+	private static partial Regex SvgRegex();
+
+}
